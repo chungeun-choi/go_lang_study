65b8865c59c9c0de5f5005ea24936d7ab2b925dc
cucuridas@gamil.com <3310223@naver.com>
2023-10-28 14:59:50 +0900

    [study] generic 프로그래밍 interface안에 타입제한자 추가

--- advanced/generic_two/limited_interface/limited.go
+++ advanced/generic_two/limited_interface/limited.go
@@ -1,1 +1,16 @@
 package limited_interface
+
+import "golang.org/x/exp/constraints"
+
+// interface에 타입 제한자를 둠으로써 제한
+type ParamOrdered interface {
+	constraints.Ordered
+	Sum(b int) int
+}
+
+// 실제 인터페이스 구현체
+type IntStruct int
+
+func (s IntStruct) Sum(b int) int {
+	return int(s) + b
+}
