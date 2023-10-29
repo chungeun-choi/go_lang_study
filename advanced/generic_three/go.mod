efa0fc7d097061ad3142f24a3da666b50aeae9f8
cucuridas@gamil.com <3310223@naver.com>
2023-10-29 17:04:47 +0900

    [study] 타입 지정 매개변수를 구조체에서 활용하기 위한 예제 코드 추가

---
+++ advanced/generic_three/go.mod
@@ -0,0 +1,3 @@
+module advanced/generic_three
+
+go 1.19
--- advanced/generic_three/main.go
+++ advanced/generic_three/main.go
@@ -1,1 +1,20 @@
-package generic_three
+package main
+
+import (
+	"advanced/generic_three/generic_type"
+	"fmt"
+)
+
+func call1() {
+	node1 := generic_type.NewNode(33)
+	node1.Push(44).Push(55).Push(66)
+
+	for node1 != nil {
+		fmt.Println(node1.Var)
+		node1 = node1.Next
+	}
+}
+
+func main() {
+	call1()
+}
